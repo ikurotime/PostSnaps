c27e5003ccb2c10adbd37fea0c94bf8376abff22
David Huertas <davidhuertasortiz@gmail.com>
2022-12-13 13:21:28 +0100

    add blue spinner fill

--- islands/TweetContainer.tsx
+++ islands/TweetContainer.tsx
@@ -49,7 +49,7 @@ export default function TweetContainer() {
             <div class="w-[40vw] h-60 grid place-items-center">
               <div role="status">
                 <svg
-                  class="inline mr-2 w-20 h-20 text-gray-200 animate-spin dark:text-gray-600"
+                  class="inline mr-2 w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                   viewBox="0 0 100 101"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
