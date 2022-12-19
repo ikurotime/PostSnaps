23e154f4b204def7fd0e644d49e32283e04027d2
David Huertas <davidhuertasortiz@gmail.com>
2022-12-19 13:46:55 +0100

    add missing date

--- components/ChangeForm.tsx
+++ components/ChangeForm.tsx
@@ -9,7 +9,7 @@ export const ChangeForm = ({ handleSubmit, handleChange, loading }: Props) => {
       <div>
         <label
           for="password"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          class="block mb-2 text-sm font-medium text-white"
         >
           New password
         </label>
@@ -18,7 +18,7 @@ export const ChangeForm = ({ handleSubmit, handleChange, loading }: Props) => {
           name="password"
           id="password"
           onInput={handleChange}
-          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class=" border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
           placeholder="name@company.com"
           required
         />
@@ -26,7 +26,7 @@ export const ChangeForm = ({ handleSubmit, handleChange, loading }: Props) => {
       <div>
         <label
           for="password_confirm"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          class="block mb-2 text-sm font-medium text-white"
         >
           Confirm new password
         </label>
@@ -35,7 +35,7 @@ export const ChangeForm = ({ handleSubmit, handleChange, loading }: Props) => {
           name="password_confirm"
           id="password_confirm"
           onInput={handleChange}
-          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
           placeholder="name@company.com"
           required
         />
