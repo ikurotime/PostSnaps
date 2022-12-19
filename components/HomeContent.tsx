7539bc7b7c1cea5838ecccce3157d5cc04b13daa
David Huertas <davidhuertasortiz@gmail.com>
2022-12-19 20:26:24 +0100

    fix "missing content"

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -4,16 +4,17 @@ import Navbar from "../islands/Navbar.tsx";
 import TweetContainer from "../islands/TweetContainer.tsx";
 
 export default function HomeContent(
-  { user, liked_post, tweetData }: {
+  { user, liked_post, tweetData, tweetText }: {
     user: User;
     liked_post: boolean;
     tweetData: any;
+    tweetText: string;
   },
 ) {
   return (
     <>
       <Navbar user={user} />
-      <TweetContainer tweetData={tweetData} />
+      <TweetContainer tweetData={tweetData} tweetText={tweetText} />
       <BottomBar user={user} liked_post={liked_post} />
     </>
   );
