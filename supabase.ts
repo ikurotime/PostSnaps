a189455910262ff1d53103712eab4129005de609
David Huertas <davidhuertasortiz@gmail.com>
2022-12-31 19:34:02 +0100

    test fix url

--- supabase.ts
+++ supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "supabase";
 import "https://deno.land/x/dotenv/load.ts";
 
 export const supabaseClient = createClient(
-  Deno.env.get("SUPABASE_URL") as string,
-  Deno.env.get("SUPABASE_KEY") as string,
+  "https://nitjkhytnaowbkuggtwa.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pdGpraHl0bmFvd2JrdWdndHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA2ODc0OTAsImV4cCI6MTk4NjI2MzQ5MH0.BZRNf7yQgX8xqsvfnNROSgh6wOsDMUvYeis2M6Kh0-g",
 );
